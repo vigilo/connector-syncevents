8036d0994b86ba2042271433982a2a2c205651bd
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-18 16:31:52 +0200

    Activation de la doc PDF / Latex
    Change-Id: I135d28e54a6b21e39c2ccd9637b3caa16756ed3d

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,12 @@
 project = u'Vigilo connector-syncevents'
 
 pdf_documents = [
-        ('admin', "connector-syncevents-admin", "Connector-syncevents : Guide d'administration", u'Vigilo'),
+        ('admin', "admin-connector-syncevents", "Connector-syncevents : Guide d'administration", u'Vigilo'),
+]
+
+latex_documents = [
+        ('admin', 'admin-connector-syncevents.tex', u"Connector-syncevents : Guide d'administration",
+         'AA100004-2/TODO', 'vigilo'),
 ]
 
 execfile("../buildenv/doc/conf.py")
