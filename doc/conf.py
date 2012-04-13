vigilo/connector-nagios

Factorisation de la doc
Change-Id: I83a6cf2303e612eb95690db154cbef1752cbccad

doc/conf.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-project = u'Vigilo connector-nagios'
+name = u'connector-nagios'
+
+project = u'Vigilo %s' % name
 
 pdf_documents = [
-        ('admin', "admin-connector-nagios", "Connector-nagios : Guide d'administration", u'Vigilo'),
-        ('dev', "dev-connector-nagios", "Connector-nagios : Guide de développement", u'Vigilo'),
+        ('admin', "admin-%s" % name, "Connector-nagios : Guide d'administration", u'Vigilo'),
+        ('dev', "dev-%s" % name, "Connector-nagios : Guide de développement", u'Vigilo'),
 ]
 
 latex_documents = [
-        ('admin', 'admin-connector-nagios.tex', u"Connector-nagios : Guide d'administration",
+        ('admin', 'admin-%s.tex' % name, u"Connector-nagios : Guide d'administration",
          'AA100004-2/ADM00002', 'vigilo'),
-        ('dev', 'dev-connector-nagios.tex', u"Connector-nagios : Guide de développement",
+        ('dev', 'dev-%s.tex' % name, u"Connector-nagios : Guide de développement",
          'AA100004-2/DEV00006', 'vigilo'),
 ]
 

vigilo/connector-syncevents

Cherry-picked from commit 6a5202e8b90ce3d047c87669b5cac0337fe02817.
Change-Id: If74b4cae0e1189b56355d25ae3cc495250a0921a
Reviewed-on: https://vigilo-dev.si.c-s.fr/review/643
Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

doc/conf.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
-project = u'Vigilo connector-syncevents'
+name = u'connector-syncevents'
+
+project = u'Vigilo %s' % name
 
 pdf_documents = [
-        ('admin', "admin-connector-syncevents", "Connector-syncevents : Guide d'administration", u'Vigilo'),
+        ('admin', "admin-%s" % name, "Connector-syncevents : Guide d'administration", u'Vigilo'),
 ]
 
 latex_documents = [
-        ('admin', 'admin-connector-syncevents.tex', u"Connector-syncevents : Guide d'administration",
+        ('admin', 'admin-%s.tex' % name, u"Connector-syncevents : Guide d'administration",
          'AA100004-2/TODO', 'vigilo'),
 ]
 