aadc9bbbba4283736f987b7c654da02890343ff6
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-07-10 18:16:55 +0200

    Bump version v5.1.0alpha2
    Change-Id: I81d572a2ecdafcfbc29fab47d422e0d7f223f325
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def install_i18n(i18ndir, destdir):
     return data_files
 
 setup(name='vigilo-connector-syncevents',
-        version='5.1.0a1',
+        version='5.1.0a2',
         author='Vigilo Team',
         author_email='contact.vigilo@c-s.fr',
         url='https://www.vigilo-nms.com/',
